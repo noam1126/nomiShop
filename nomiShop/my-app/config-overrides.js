450a1b0179411737b559d67d6bceaf04158d4880
Noam Nachshon <noam1126@gmail.com>
2024-07-25 13:54:39 +0300

    update

--- nomiShop/my-app/config-overrides.js
+++ nomiShop/my-app/config-overrides.js
@@ -2,8 +2,20 @@ const webpack = require("webpack");
 
 module.exports = function override(config, env) {
   config.resolve.fallback = {
-    ...config.resolve.fallback,
     zlib: require.resolve("browserify-zlib"),
+    querystring: require.resolve("querystring-es3"),
+    path: require.resolve("path-browserify"),
+    crypto: require.resolve("crypto-browserify"),
+    stream: require.resolve("stream-browserify"),
+    http: require.resolve("stream-http"),
+    process: require.resolve("process/browser"),
+    fs: false,
+    net: false,
   };
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    }),
+  ]);
   return config;
 };
