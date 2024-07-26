0c88dcae7c8755e79879872e78e0992ffacaf32f
Noam Nachshon <noam1126@gmail.com>
2024-07-26 10:12:26 +0300

    h

--- nomiShop/my-app/src/App.js
+++ nomiShop/my-app/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Signup from "./Signup";
+import Login from "./Login";
+import Home from "./Components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <BrowserRouter>
+    <Router>
+      <Route path="/register" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+    </Router>
+    // </BrowserRouter>
   );
 }
 
